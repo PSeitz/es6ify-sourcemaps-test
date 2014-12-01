05a32e7c148bde0b85f6be3ca723e32abe23afda
Shuvalov Anton <anton_shuvalov@me.com>
2014-12-01 17:08:44 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,19 @@
  * Dependencies
  */
 var gulp        = require('gulp');
-var browserify  = require('gulp-browserify');
 var regenerator = require('gulp-regenerator');
 var jadeify     = require('jadeify');
+var es6ify      = require('es6ify');
+var browserify  = require('browserify');
+var fs          = require('fs');
 
 
 gulp.task('default', function() {
-  gulp.src('index.js')
-    .pipe(browserify({ debug: true }))
-    .on('prebundle', function(bundle) {
-      bundle
-        .transform(jadeify)
-        .add('./third.js')
-      ;
-    })
-    .on('error', function(e) { console.log(e); })
-    .pipe(regenerator())
-    .pipe(gulp.dest('dest'))
-  ;
+  browserify({ debug: true })
+    .add(es6ify.runtime)
+    .transform(es6ify)
+    .transform(jadeify)
+    .require('./third.js', { entry: true })
+    .bundle()
+    .pipe(fs.createWriteStream('./dest/index.js'));
 });
